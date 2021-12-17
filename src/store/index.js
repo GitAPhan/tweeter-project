8c91f6f38c322b9b76c21d45797115bfc4f973fc
GitAPhan <itsandrewphan@gmail.com>
2021-12-16 18:06:45 -0700

    Created the skeleton of a new project

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import cookies from 'vue-cookies'
+import axios from 'axios'
+
+axios
+cookies
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})

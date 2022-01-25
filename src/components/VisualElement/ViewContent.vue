192507e394893de55fa521ace922849a8cc43834
GitAPhan <itsandrewphan@gmail.com>
2022-01-25 14:36:35 -0700

    go_to_profile() {

---
+++ src/components/VisualElement/ViewContent.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <!-- tweet format -->
+    <article class="tweetCard" v-if="this.content_type == 'tweet'">
+      <h3 class="tweetContent">{{ tweet.content }}</h3>
+      <img
+        class="tweetImage"
+        v-if="this.tweet.imageUrl !== null || this.tweet.tweetImageUrl !== null"
+        :src="tweet.tweetImageUrl"
+        alt="tweeted picture"
+      />
+
+      <div class="userInfo">
+        <img
+          @click="go_to_profile"
+          class="profileImage"
+          :src="tweet.userImageUrl"
+          alt="user profile picture"
+        />
+        <h6 @click="go_to_profile" class="tweetInfo">
+          {{ tweet.username }}, {{ new Date(tweet.createdAt) }}
+        </h6>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "view-content",
+  props: {
+    // choice of 'tweet', 'comment' & 'profile'
+    content_type: String,
+    // tweet content
+    tweet: Object,
+  },
+  methods: {
+    reformat_tweet() {
+      var tweet = this.unfiltered_tweet;
+      // change key name from imageUrl to tweetImageUrl to match GET data return
+      if (tweet.imageUrl !== null) {
+        tweet[tweetImageUrl] = tweet.imageUrl;
+      }
+    },
+    go_to_profile() {
+      // go to profile function.
+      this.$router
+        .push({
+          name: "ProfilePage",
+          params: {
+            // userId is passed to the profilePage to determine which profile to show
+            userId: this.tweet.userId,
+          },
+        })
+        .catch(() => {
+            // if user is already on a profile page
+            // this will send the user to the feed page and then back to profile page with new params
+          this.$router
+            .push({
+              name: "FeedPage",
+            })
+            .then(() => {
+              this.$router.push({
+                name: "ProfilePage",
+                params: {
+                  // userId is passed to the profilePage to determine which profile to show
+                  userId: this.tweet.userId,
+                },
+              });
+            });
+        });
+    },
+  },
+  mounted() {
+    this.reformat_tweet();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/z-RequestElement/TweetRequest.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+      <view-content
+      content_type='tweets'
+      v-for="tweet of tweets"
+      :key="tweet.tweetId"
+      :unfiltered_tweet="tweet"
+      ></view-content>
+      <!-- submit content would have priority when active -->
+        <submit-content>
+        </submit-content>
+  </div>
+</template>
+
+<script>
+import ViewContent from "@/components/VisualElement/ViewContent.vue";
+import SubmitContent from "@/components/VisualElement/SubmitContent.vue";
+
+export default {
+  name: "tweet-request",
+  components: {
+      ViewContent,
+      SubmitContent,
+  },
+  methods: {
+    //   all actions related to storing tweets are stored here
+    grab_tweets(payload) {
+      // store tweets here
+      var tweets = [];
+      // tweet params
+      var tweet_params = {};
+
+      // figure out params
+      if (this.userId != "undefined") {
+        // userId is a prop passed down from the container
+        tweet_params = {
+          params: {
+            userId: this.userId,
+          },
+        };
+      }
+
+      // request to grab tweets
+      this.$axios
+        .request({
+          url: "https://tweeterest.ga/api/tweets",
+          tweet_params,
+        })
+        .then((response) => {
+          tweets = response.data;
+          //   order tweets by time
+          tweets.sort((a, b) => {
+            if (a.createdAt < b.createdAt) {
+              return 1;
+            }
+            if (a.createdAt > b.createdAt) {
+              return -1;
+            }
+            return 0;
+          });
+          //   store ordered tweets in data
+          this.tweets = tweets;
+        })
+        .catch((error) => {
+          console.log(error);
+          error;
+        });
+    },
+    // Post request tweets
+    post_request(payload) {
+      // variables
+      var tweet = {};
+      var old_tweet_index = {};
+      var content = undefined;
+      var imageUrl = undefined;
+      var tweets = this.tweets;
+      // request data
+      var request_data = {};
+      var request_method = undefined;
+
+      // axios request
+      this.$axios
+        .request({
+          url: "https://tweeterest.ga/api/tweets",
+          method: request_method,
+          data: request_data,
+        })
+        .then((response) => {
+          // tweet stored here
+          tweet = response.data; // need to run a delete to view what the response looks like
+        })
+        .catch((error) => {
+          error;
+        });
+
+      //   to only run if the request_method is POST
+      if (request_method === "POST") {
+        // adding tweet to this.tweet array
+        tweets.unshift(tweet);
+      }
+
+      //   to only run if the request_method is PATCH
+      if (request_method === "PATCH") {
+        // find index of tweet
+        old_tweet_index = tweets.findIndex((x) => x.tweetId === tweet.tweetId);
+        // replace tweet
+        tweets[old_tweet_index]['content'] = tweet.content;
+        // replace tweet image only if present
+        if(tweet.imageUrl !== null) {
+            tweets[old_tweet_index]['tweetImageUrl'] = tweet.imageUrl;
+        }
+      }
+
+      //   to only run if the request_method is DELETE
+      if (request_method === "DELETE") {
+        // find index of tweet
+        old_tweet_index = tweets.findIndex((x) => x.tweetId === tweet.tweetId);
+        // delete tweet from array
+        tweets.splice(old_tweet_index, 1);
+      }
+
+      //   store ordered tweets in data
+      this.tweets = tweets;
+    },
+  },
+  data() {
+    return {
+      // only ordered tweets are stored here
+      tweets: [],
+      loginToken: [this.$cookies.get("loginToken")],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
